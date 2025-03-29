c09ad53088e270e33a119a1c14ee6b99a7f21eb3
Orest Yakymiv <akimivorest@gmail.com>
2025-03-29 14:13:44 +0200

    create function copy_file which check if our file exist and copy dat from this file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import shutil
+import os
+
+
+def copy_file(command):
+
+    split_command = command.split()
+
+    if len(split_command) != 3 or split_command[0] != "cp":
+        return
+
+    source_filename, target_filename = split_command[1], split_command[2]
+
+    if os.path.exists(source_filename) and source_filename != target_filename:
+        shutil.copyfile(source_filename, target_filename)
+    else:
+        return
